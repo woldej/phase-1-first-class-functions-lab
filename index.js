15bb2b3bb7f77c5edda03c7477381a63a821db47
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-06 10:27:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers=function(theArray){return theArray.slice(0,2)} ;
+
+const returnLastTwoDrivers=function (theArray){return theArray.slice(-2)};
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return function(){return x*5}
+}
+function fareDoubler(y){
+    const createFareMultiplier=function(){return y*2}
+    return createFareMultiplier()
+}
+
+function fareTripler(z){
+    const createFareMultiplier=function(){return z*3}
+    return createFareMultiplier()
+}
+
+const selectDifferentDrivers=function(theArray,returnLastTwoDrivers){
+
+    return returnLastTwoDrivers(theArray)
+}
